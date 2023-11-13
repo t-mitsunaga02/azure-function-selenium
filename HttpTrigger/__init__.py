46b5044ad768380e2810e095aba9a5a08aa63d26
t-mitsunaga02 <t-mitsunaga@analytics-jp.com>
2023-11-13 14:07:51 +0900

    modify commit

--- HttpTrigger/__init__.py
+++ HttpTrigger/__init__.py
@@ -37,7 +37,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     credential = DefaultAzureCredential()
     storage_account_url = "https://" + os.environ["par_storage_account_name"] + ".blob.core.windows.net"
     client = BlobServiceClient(account_url=storage_account_url, credential=credential)
-    blob_name = "test" + str(datetime.now()) + ".txt"
+    blob_name = "testscrape" + str(datetime.now()) + ".txt"
     blob_client = client.get_blob_client(container=os.environ["par_storage_container_name"], blob=blob_name)
     blob_client.upload_blob(link_list)
 
