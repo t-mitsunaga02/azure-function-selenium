a1affcf8ba61482b5611c671e86c3a1bc8f342cb
t-mitsunaga02 <t-mitsunaga@analytics-jp.com>
2023-11-13 14:45:04 +0900

    modify commit

--- HttpTrigger/__init__.py
+++ HttpTrigger/__init__.py
@@ -7,10 +7,39 @@ from selenium.webdriver.common.by import By
 from azure.identity import DefaultAzureCredential, ClientSecretCredential
 from azure.storage.blob import BlobServiceClient
 from datetime import datetime
+import pandas as pd
 import os
+import io
+
+#新しいクラス（モジュール）を作る 
+class Scrape():
+ 
+    def __init__(self,wait=1,max=None):
+        self.response = None
+        self.df = pd.DataFrame()
+        self.wait = wait
+        self.max = max
+        self.headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36"}
+        self.timeout = 5
+ 
+    def to_csv(self,filename,dropcolumns=None):
+        '''
+        DataFrame をCSVとして出力する
+        dropcolumns に削除したい列をリストで指定可能
+ 
+        Params
+        ---------------------      
+        filename:str
+            ファイル名
+        dropcolumns:[str]
+            削除したい列名            
+        '''
+        if dropcolumns is not None:
+            self.df.drop(dropcolumns,axis=1,inplace=True) 
+        self.df.to_csv(filename,index=False,encoding="shift-jis",errors="ignore")
+  
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request. 12:38')
 
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')
@@ -19,9 +48,9 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     service = Service(executable_path=r"/usr/local/bin/chromedriver")
 
     driver = webdriver.Chrome(service=service, options=options)
-    logging.info(driver)
     driver.get('http://abehiroshi.la.coocan.jp/')
     links = driver.find_elements(By.TAG_NAME, "a")
+    logging.warn(links)
     link_list = ""
     for link in links:
         if link_list == "":
@@ -29,13 +58,19 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         else:
             link_list = link_list + ", " + link.text
 
+
+    # データフレームをCSV形式の文字列に変換し、その文字列をメモリ上のストリームに書き込む
+    csv_buffer = io.StringIO()
+    link_list.to_csv(csv_buffer, encoding='utf_8', index=False)
+
+
     # # create blob service client and container client
     # credential = DefaultAzureCredential()
     # storage_account_url = "https://" + os.environ["par_storage_account_name"] + ".blob.core.windows.net"
     # client = BlobServiceClient(account_url=storage_account_url, credential=credential)
     blob_name = "testscrape" + str(datetime.now()) + ".txt"
 
-    logging.info(link_list)
+    logging.warn(link_list)
 
     # BLOBへの接続
     connect_str = os.getenv("AzureWebJobsStorage")
