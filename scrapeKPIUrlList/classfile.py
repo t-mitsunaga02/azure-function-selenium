04e4d8e534563d141c9d308d1c9a21c025128f2d
t-mitsunaga02 <t-mitsunaga@analytics-jp.com>
2023-11-24 20:48:23 +0900

    modify commit

---
+++ scrapeKPIUrlList/classfile.py
@@ -0,0 +1,96 @@
+
+import pandas as pd
+
+#新しいクラス（モジュール）を作る 
+class Scrape():
+ 
+    def __init__(self,wait=1,max=None):
+        self.response = None
+        self.df = pd.DataFrame()
+        self.wait = wait
+        self.max = max
+        self.headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36"}
+        self.timeout = 5
+ 
+    def to_csv(self,filename,dropcolumns=None):
+        '''
+        DataFrame をCSVとして出力する
+        dropcolumns に削除したい列をリストで指定可能
+ 
+        Params
+        ---------------------      
+        filename:str
+            ファイル名
+        dropcolumns:[str]
+            削除したい列名            
+        '''
+        if dropcolumns is not None:
+            self.df.drop(dropcolumns,axis=1,inplace=True) 
+        self.df.to_csv(filename,index=False,encoding="shift-jis",errors="ignore")
+
+    def add_df(self,values,columns,omits = None):
+        '''
+        指定した値を　DataFrame に行として追加する
+        omits に削除したい文字列をリストで指定可能
+ 
+        Params
+        ---------------------      
+        values:[str]
+            列名
+        omits:[str]
+            削除したい文字、又は文字列            
+        '''
+        if omits is not None:
+            values = self.omit_char(values,omits)
+            columns = self.omit_char(columns,omits)
+        
+        df = pd.DataFrame(values,index=self.rename_column(columns))
+        self.df = pd.concat([self.df,df.T])
+
+    def omit_char(self,values,omits):
+        '''
+        リストで指定した文字、又は文字列を削除する
+ 
+        Params
+        ---------------------      
+        values:str
+            対象文字列
+        omits:str
+            削除したい文字、又は文字列            
+ 
+        Returns
+        ---------------------
+        return :str
+            不要な文字を削除した文字列
+        '''
+        for n in range(len(values)):
+            for omit in omits:
+                values[n] = values[n].replace(omit,'')
+        return values
+
+    def rename_column(self,columns):
+        '''
+        重複するカラム名の末尾に連番を付与し、ユニークなカラム名にする
+            例 ['A','B','B',B'] → ['A','B','B_1','B_2']
+ 
+        Params
+        ---------------------      
+        columns: [str]
+            カラム名のリスト
+          
+        Returns
+        ---------------------
+        return :str
+            重複するカラム名の末尾に連番が付与されたリスト
+        '''
+        lst = list(set(columns))
+        for column in columns:
+            dupl = columns.count(column)
+            if dupl > 1:
+                cnt = 0
+                for n in range(0,len(columns)):
+                    if columns[n] == column:
+                        if cnt > 0:
+                            columns[n] = f'{column}_{cnt}'
+                        cnt += 1
+        return columns
