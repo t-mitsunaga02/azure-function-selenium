e18259d07fe29a817dca824c421e8e6e6f6ea7fd
t-mitsunaga02 <t-mitsunaga@analytics-jp.com>
2023-11-30 17:32:27 +0900

    modify commit

--- scrapeKPIUrlList/classfile.py
+++ scrapeKPIUrlList/classfile.py
@@ -3,6 +3,13 @@ import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
+import requests
+from bs4 import BeautifulSoup
+import random
+import datetime
+import time
+import pandas as pd
+
 #新しいクラス（モジュール）を作る 
 class Scrape():
  
@@ -24,9 +31,45 @@ class Scrape():
         options.add_argument('--headless')
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
+        options.add_argument('--disable-gpu')
+        options.add_argument('--disable-web-security')
         ## Dockerにあるchromedriverを使用
         service = Service(executable_path=r"/usr/local/bin/chromedriver")
         return webdriver.Chrome(service=service, options=options)
+
+    def request(self,url,wait=None,max=None,console=True):
+        '''
+        指定したURLからページを取得する。
+        取得後にwaitで指定された秒数だけ待機する。
+        max が指定された場合、waitが最小値、maxが最大値の間でランダムに待機する。
+
+        Params
+        ---------------------
+        url:str
+            URL
+        wait:int
+            ウェイト秒
+        max:int
+            ウェイト秒の最大値
+        console:bool
+            状況をコンソール出力するか
+        Returns
+        ---------------------
+        soup:BeautifulSoupの戻り値
+        '''
+        self.wait = self.wait if wait is None else wait
+        self.max = self.max if max is None else max
+
+        start = time.time()
+        response = requests.get(url,headers=self.headers,timeout = self.timeout)
+        time.sleep(random.randint(self.wait,self.wait if self.max is None else self.max))
+
+        if console:
+            tm = datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')
+            lap = time.time() - start
+            print(f'{tm} : {url}  経過時間 : {lap:.3f} 秒')
+
+        return BeautifulSoup(response.content, "html.parser")
 
     def to_csv(self,filename,dropcolumns=None):
         '''
--- scrapeKPIUrlList/get_url_amazon.py
+++ scrapeKPIUrlList/get_url_amazon.py
@@ -2,9 +2,8 @@ import logging
 from .classfile import Scrape
 
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 import time
-import pandas as pd
+import re
 from urllib.parse import urlparse
 
 def get_url_amazon(get_pos):
@@ -21,49 +20,81 @@ def get_url_amazon(get_pos):
         ## メーカー・製品名の抽出
         search_word = f"{row['BRAND']} {row['Item']}"
 
-        ## Amazon検索
-        ### GoogleからAmazonの検索ページを検索する
-        driver.get(f"https://www.google.com/search?q=https://www.amazon.co.jp/s?k={row['BRAND']}+{row['Item']}")
+        # Amazon検索
+        ## Amazonの検索ページを表示する
+        target = f"https://www.amazon.co.jp/s?k={row['BRAND']}+{row['Item']}&crid=3M1NNK3XMSY5M&sprefix=kc-n50%2Caps%2C174&ref=nb_sb_noss_1"
 
-        ### 検索結果ページがロードされるのを待つ（例: 3秒待つ）
+        scr = Scrape(wait=2,max=5)
+        soup = scr.request(target)
+
+        ## 検索結果ページがロードされるのを待つ（例: 3秒待つ）
         time.sleep(3)
 
-        ### 検索結果のリンクを収集
-        links = driver.find_elements(By.CSS_SELECTOR, "h3")
+        ## 製品ページのタグを取得
+        shop_url = soup.find_all('a',class_='a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal')
 
-        #検索結果を１つずつみて、リンク先のURLを抽出
-        for link in links:
+        ## 検索結果を１つずつみて、該当の製品ID（asin）を取得するループ
+        for link in shop_url:
+            ### 製品のタイトルを取得
+            link_text = link.find('span', class_='a-size-base-plus a-color-base a-text-normal')
+            if link_text:
+                ### 検索した製品と同名かどうか
+                if row['Item'] not in link_text.text: 
+                    continue
+                ### フィルタ製品じゃないかどうか
+                if "フィルタ" in link_text.text:
+                    continue
+                ### 製品ページのURLからasinIDを取得
+                asin_url = link.get('href')
+                match = re.search(r'/dp/(\w{10})', asin_url)
+                asin_string = match.group(1)
+                print(asin_string)
 
-            link_url = link.find_element(By.XPATH, '..').get_attribute('href')
-            logging.info("link:")
-            logging.info(link_url)
-            ## 製品名を含まないなら除外
-            if row['Item'] not in link_url:
+                #DataFrameに登録
+                columns = ['BRAND','Item','asinID']
+                values = [row['BRAND'],row['Item'],asin_string] 
+                scr.add_df(values,columns)
+
+            else:
                 continue
-            logging.info("1")
-            ## 製品ページのパスがないなら除外
-            if "/dp/" not in link_url:
-                continue
-            logging.info("2")
-            ## 英語ページなら除外
-            if "/-/en/" in link_url:
-                continue
-            logging.info("3")
-            ## フィルタ製品ページなら除外
-            if "フィルタ―" in link_url:
-                continue
-            logging.info("4")
 
-            parsed_url = urlparse(link_url).path
+        ## 製品ページへ遷移
+        driver = scr.get_driver()
+        driver.get(f"https://www.amazon.co.jp{asin_url}")
 
-            # 検索文字列の位置を見つける
-            pos = parsed_url.find("/dp/")
+        ## 色・スタイルの切り替えのためリンクを取得
+        li_elements = driver.find_elements(By.CSS_SELECTOR, "li[data-csa-c-content-id='twister-desktop-configurator-swatch-swatchAvailable']")
 
-            asinID = link_url[pos + len("/dp/"): pos + len("/dp/") + 10]
+        ## 色・スタイルのリンク分ループ
+        for li in li_elements:
+            ### リンクの押下
+            submit_button = li.find_element(By.CSS_SELECTOR, "input[type='submit']")
+            submit_button.click()
+
+            ### 新しいページがロードされるのを待機
+            time.sleep(3)
+
+            ### 切り替え後の製品タイトル取得
+            color_text = driver.find_element(By.CSS_SELECTOR, "span.a-size-large.product-title-word-break")
+            print(f"color:{color_text.text}")
+
+            ### 切り替え後のタイトルが検索した製品と同名かどうか
+            if row['Item'] not in color_text.text: 
+                continue
+            ### 切り替え後のタイトルがフィルタ製品じゃないかどうか
+            if "フィルタ" in color_text.text:
+                continue
+
+            ### 製品ページのURLからasinIDを取得
+            match = re.search(r'/dp/(\w{10})', driver.current_url)
+            asin_string = match.group(1)
+
+            print(f"mojiretu:{asin_string}")
 
             #DataFrameに登録
-            columns = ['ID','BRAND','Item','asinID']
-            values = [row['ID'],row['BRAND'],row['Item'],asinID] 
+            columns = ['BRAND','Item','asinID']
+            values = [row['BRAND'],row['Item'],asin_string] 
             scr.add_df(values,columns)
-
+            scr.df.drop_duplicates()
+        driver.quit()
     return scr
