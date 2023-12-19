010632b12d513a0554824a4c41b20cee17d81703
t-mitsunaga02 <t-mitsunaga@analytics-jp.com>
2023-12-19 16:58:00 +0900

    modify commit

--- scrapeAmazonData/class_file.py
+++ scrapeAmazonData/class_file.py
@@ -29,7 +29,7 @@ class Scrape():
             Chrome接続情報        
         '''
         options = webdriver.ChromeOptions()
-        options.add_argument('--headless')
+        # options.add_argument('--headless')
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
         options.add_argument('--disable-gpu')
