8e4756eb00a816ec1af621fd7ffc366b4f949f43
t-mitsunaga02 <t-mitsunaga@analytics-jp.com>
2023-12-19 17:15:00 +0900

    modify commit

--- scrapeAmazonData/class_file.py
+++ scrapeAmazonData/class_file.py
@@ -29,11 +29,11 @@ class Scrape():
             Chrome接続情報        
         '''
         options = webdriver.ChromeOptions()
-        # options.add_argument('--headless')
-        options.add_argument('--no-sandbox')
-        options.add_argument('--disable-dev-shm-usage')
-        options.add_argument('--disable-gpu')
-        options.add_argument('--disable-web-security')
+        options.add_argument('--headless')
+        # options.add_argument('--no-sandbox')
+        # options.add_argument('--disable-dev-shm-usage')
+        # options.add_argument('--disable-gpu')
+        # options.add_argument('--disable-web-security')
         options.add_argument('--user-agent=' + 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36')
         ## Dockerにあるchromedriverを使用
         service = Service(executable_path=r"/usr/local/bin/chromedriver")
